4207c681feee2f88783d9414ec4d2b84c74d0775
Abed001 <abed.alroufe@gmail.com>
2024-01-06 23:49:22 +0200

    finished cards

--- src/App.jsx
+++ src/App.jsx
@@ -28,12 +28,3 @@ function App() {
 }
 
 export default App;
-/*
-
-
-Greg Hooper
-
-<div class="attribution">
-  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-  Coded by <a href="#">Your Name Here</a>.
-</div>*/
